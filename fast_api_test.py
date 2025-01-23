852bbda42073a2673d002fb0e191a7b5fecbbd12
Ricardo Marques <dataricardom@gmail.com>
2025-01-23 18:31:14 -0300

    Criando API com FastAPI

---
+++ fast_api_test.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from faker import Faker
+import pandas as pd
+
+app = FastAPI(debug=True)
+fake = Faker()
+
+@app.get("/")
+async def hello_world():
+    return "Hello World"
